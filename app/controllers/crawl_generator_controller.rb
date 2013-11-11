b23f87bc86ea1e4c7cba2dc617d20e3a94df2ba3
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2013-11-11 20:17:35 +0000

    added list of pubs from google. changed a bit the formula and cleaned up the controller

--- app/controllers/crawl_generator_controller.rb
+++ app/controllers/crawl_generator_controller.rb
@@ -11,27 +11,40 @@ class CrawlGeneratorController < ApplicationController
 		selected_pubs = []
 		object = {}
 		object [:name] = current_pub[:name]
-		object [:latitude] = current_pub [:latitude]
-		object [:longitude] = current_pub[:longitude]
+		object [:latitude] = current_pub [:latitude].to_f
+		object [:longitude] = current_pub[:longitude].to_f
+		object [:distance] = 0.0
 		selected_pubs.push ( object )
 
-		for i in 0..5 do
+		for i in 0..3 do
 
+			pubs.shuffle!
 			min_distance = 10000000
 			current_pub = nil
+
+			l = (limit-i)/10.0
+
 			pubs.each do |pub|
 
-				distance = view_context.computeDistance( selected_pubs.last , pub )
-				if ( distance < min_distance )
+				object = {}
+				object [:name] = pub[:name]
+				object [:latitude] = pub[:latitude].to_f
+				object [:longitude] = pub[:longitude].to_f
+				distance = view_context.computeDistance( selected_pubs.last , object )
+
+				# puts pub[:name] + " " + selected_pubs.last[:name] + " " + distance.to_s
+
+				if ( distance < min_distance && limit < i && i < 2*limit)
 					current_pub = pub
 					min_distance = distance
 				end
+
 			end
 
 			object = {}
 			object [:name] = current_pub[:name]
-			object [:latitude] = current_pub [:latitude]
-			object [:longitude] = current_pub[:longitude]
+			object [:latitude] = current_pub [:latitude].to_f
+			object [:longitude] = current_pub[:longitude].to_f
 			object [:distance] = min_distance
 			selected_pubs.push ( object )
 			pubs.delete current_pub
--- app/helpers/crawl_generator_helper.rb
+++ app/helpers/crawl_generator_helper.rb
@@ -5,14 +5,14 @@ module CrawlGeneratorHelper
 
 	def computeDistance (p1, p2)
 		r = 6371; # earth's mean radius in km
-		dLat  = rad(p2["latitude"].to_i - p1["latitude"].to_i)
-		dLng = rad(p2["longitude"].to_i - p1["longitude"].to_i)
+		dLat  = rad(p2[:latitude] - p1[:latitude])
+		dLng = rad(p2[:longitude] - p1[:longitude])
 
 		a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-			Math.cos(rad(p1["latitude"].to_i)) * Math.cos(rad(p2["latitude"].to_i)) * Math.sin(dLng/2) * Math.sin(dLng/2)
+			Math.cos(rad(p1[:latitude])) * Math.cos(rad(p2[:latitude])) * Math.sin(dLng/2) * Math.sin(dLng/2)
 		c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
 		d = r * c;
-
+		return d.round(1)
 		return ((d.round(1))/10).round(1)
 	end
 
