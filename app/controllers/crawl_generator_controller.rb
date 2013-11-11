2dd1aa2914831aa09648d8e3334bd9032b6abf97
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2013-11-11 21:45:27 +0000

    working on getting the generator to do the job

--- app/controllers/crawl_generator_controller.rb
+++ app/controllers/crawl_generator_controller.rb
@@ -22,7 +22,7 @@ class CrawlGeneratorController < ApplicationController
 			min_distance = 10000000
 			current_pub = nil
 
-			l = (limit-i)/10.0
+			l = (limit-i)/7.0
 
 			pubs.each do |pub|
 
@@ -33,23 +33,33 @@ class CrawlGeneratorController < ApplicationController
 				distance = view_context.computeDistance( selected_pubs.last , object )
 
 				# puts pub[:name] + " " + selected_pubs.last[:name] + " " + distance.to_s
-
-				if ( distance < min_distance && limit < i && i < 2*limit)
+				puts distance
+				if ( distance < min_distance && l < distance )
 					current_pub = pub
 					min_distance = distance
 				end
 
 			end
 
-			object = {}
-			object [:name] = current_pub[:name]
-			object [:latitude] = current_pub [:latitude].to_f
-			object [:longitude] = current_pub[:longitude].to_f
-			object [:distance] = min_distance
-			selected_pubs.push ( object )
-			pubs.delete current_pub
+			if ( current_pub.nil? )
+				pub = pubs.delete pubs.sample
+				object = {}
+				object [:name] = pub[:name]
+				object [:latitude] = pub[:latitude].to_f
+				object [:longitude] = pub[:longitude].to_f
+				distance = view_context.computeDistance( selected_pubs.last , object )
+				object [:distance] = distance
+				selected_pubs.push ( object )
+			else
+				object = {}
+				object [:name] = current_pub[:name]
+				object [:latitude] = current_pub [:latitude].to_f
+				object [:longitude] = current_pub[:longitude].to_f
+				object [:distance] = min_distance
+				selected_pubs.push ( object )
+				pubs.delete current_pub
+			end
 
-			limit = limit - 1
 		end
 
 		render json: { "pubs" => selected_pubs }
